879d3d948bb5d723e1fe8e8698a7c577c08ff781
Alander <alander@tzoom.pro>
2017-03-16 18:08:12 +0400

    empty xls download released

--- src/main/resources/js/mreport.js
+++ src/main/resources/js/mreport.js
@@ -1,14 +1,21 @@
-function submitReportForm() {
+/*function submitReportForm() {
+    $( "#special_report" ).submit();
  	  var msg   = $('#special_report').serialize();
         $.ajax({
           type: 'GET',
           url: 'getDataCustomTimed',
           data: msg,
           success: function(data) {
-            $('#reportResult').html("Отчет успешно сформирован");
+            $('#reportResult').html("Report succeed prepared..."+data);
+            $.fileDownload($(this).attr('action'), {
+                    preparingMessageHtml: "We are preparing your report, please wait...",
+                    failMessageHtml: "There was a problem generating your report, please try again.",
+                    httpMethod: "POST",
+                    data: $(this).serialize()
+                });
           },
           error:  function(xhr, str){
-	        alert('Возникла ошибка: ' + xhr.responseCode);
+	        alert('Error detected: ' + xhr.responseCode);
           }
         });
-      }
+      }*/
